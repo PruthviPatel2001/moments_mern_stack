1e75fd8bde4478c1b13a12da29d9c96f04886cb8
PruthviPatel2001 <patelpruthvi.01221@gmail.com>
2022-05-25 08:54:38 +0530

    fixes

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -29,24 +29,20 @@ export default (state = { isLoading:true , posts:[] }, action) => {
 
     case actionTypes.CREATE:
 
-      console.log("reducer", action.payload);
       return {...state, posts : [...state.posts, action.payload] }
 
     case actionTypes.UPDATE:
 
-      console.log("reducer post id update", action.payload._id);
 
 
       return {...state, posts: state.posts.map((post) => post._id === action.payload._id ? action.payload : post)}
 
     case actionTypes.DELETE:
 
-      console.log("reducer post id update", action.payload);
       return {...state , posts: state.posts.filter((post) => post._id !== action.payload)}
 
     case actionTypes.LIKE:
 
-      console.log("reducer post id update", action.payload._id);
 
       return {...state, posts: state.posts.map((post) => post._id === action.payload._id ? action.payload : post)}
 
